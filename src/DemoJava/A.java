29909ed160d1105d71adc1a87249685655d39959
biswajit <biswajit.sahoo@apmosys.cmo>
2023-08-27 22:31:58 +0530

    DemoJava

---
+++ src/DemoJava/A.java
@@ -0,0 +1,15 @@
+package DemoJava;
+
+public class A {
+	
+static	public A() {
+		System.out.println("fuyutyty");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a=new A();
+
+	}
+
+}
